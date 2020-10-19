ef4cf7707aa9316d0f03c8071365c694b5d5b69b
ahmed-lahmar <ahmedlahmar1920@gmail.com>
2020-10-19 19:13:36 +0100

    19h

--- main.js
+++ main.js
@@ -38,5 +38,35 @@ var questions = [
   ]
 
 
+ var score = 0;
+  
+ function answer (object){
+    $('body').append('<div id="questions" class="q1" >'+object.question+'</div>');
+    $('#questions').append('<div class="container"></div>');
+    $('.container').append('<button id="ans2"  class="ans2-btn" onclick="nextQuestion">'+object.answers[0].text+'</button>');
+    $('.container').append('<button id="ans1"  class="ans1-btn" onclick="nextQuestion">'+object.answers[1].text+'</button>');
+    $('.container').append('<button id="ans3"  class="ans3-btn" onclick="nextQuestion">'+object.answers[2].text+'</button>');
+    $('.container').append('<button id="ans4"  class="ans4-btn" onclick="nextQuestion">'+object.answers[3].text+'</button>');
+
+}
+
+
+
+ answer({
+    question: 'In what part of the body would you find the fibula?',
+    answers: [
+      { text: 'arm', correct: false },
+      { text: 'leg', correct: true },
+      { text: 'head', correct: false },
+      { text: 'stomach', correct: false }
+    ]
+  })
+ 
+
+  function  all( array) {
+      
+  }
+
+
   
 
