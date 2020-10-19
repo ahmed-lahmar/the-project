d003188a1698de7caeca1ae4b7d6c42e24a57ba8
ahmed-lahmar <ahmedlahmar1920@gmail.com>
2020-10-19 15:04:25 +0100

    15h

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-$('#main').hide
+var questions = [
+    {
+      question: 'What is 6*6?',
+      answers: [
+        { text: '36', correct: true },
+        { text: '22', correct: false },
+        { text: '32', correct: false },
+        { text: '24', correct: false }
+      ]
+    },
+    {
+      question: 'If you have cryophobia, what are you afraid of?',
+      answers: [
+        { text: 'Ice/cold', correct: true },
+        { text: 'heights', correct: false },
+        { text: 'speed', correct: false },
+        { text: 'darkness', correct: false }
+      ]
+    },
+    {
+      question: 'What is seven cubed?',
+      answers: [
+        { text: '42', correct: false },
+        { text: '343', correct: true },
+        { text: '77', correct: false },
+        { text: '56', correct: false }
+      ]
+    },
+    {
+      question: 'In what part of the body would you find the fibula?',
+      answers: [
+        { text: 'arm', correct: false },
+        { text: 'leg', correct: true },
+        { text: 'head', correct: false },
+        { text: 'stomach', correct: false }
+      ]
+    }
+  ]
+
+
+  
+
